134e76837a5322a70eae76c9e3592c3e2fbd3ab2
karreypradeep <kpgoud533@gmail.com>
2016-08-25 00:06:26 +0530

    Contact Service implementation.

--- CollaboratorWebApp/src/main/java/com/bluespacetech/CollaboratorWebAppApplication.java
+++ CollaboratorWebApp/src/main/java/com/bluespacetech/CollaboratorWebAppApplication.java
@@ -1,23 +1,37 @@
 package com.bluespacetech;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableFeignClients
+@IntegrationComponentScan
 public class CollaboratorWebAppApplication {
 
     @LoadBalanced
@@ -31,11 +45,55 @@ public class CollaboratorWebAppApplication {
     }
 }
 
+
+@MessagingGateway
+interface ContactWriter {
+    @Gateway(requestChannel = "output")
+    void write(String rn);
+}
+
+interface ContactChannels {
+    @Output
+    MessageChannel output();
+}
+
+@FeignClient("contactservice")
+interface ContactReader {
+    @RequestMapping(method = RequestMethod.GET, value = "/contacts")
+    List<Contact> read();
+}
+
 @RestController
 @RequestMapping("/contacts")
 class ContactApiGatewayRestController {
 
+    private final ContactReader contactReader;
+
+    private final ContactWriter contactWriter;
+
     @Autowired
+    public ContactApiGatewayRestController(final ContactReader contactReader, final ContactWriter contactWriter) {
+	this.contactReader = contactReader;
+	this.contactWriter = contactWriter;
+    }
+
+    public Collection<String> fallback() {
+	return new ArrayList<>();
+    }
+
+    // @HystrixCommand(fallbackMethod = "fallback")
+    @RequestMapping(method = RequestMethod.GET, value = "/names")
+    public ResponseEntity<List<Contact>> names() {
+	List<Contact> contacts = this.contactReader.read();
+	return new ResponseEntity<List<Contact>>(contacts, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void write(@RequestBody final Contact contact) {
+	this.contactWriter.write(contact.getFirstName());
+    }
+
+    /*@Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping("")
@@ -43,7 +101,7 @@ class ContactApiGatewayRestController {
 	Contact[] contacts  = restTemplate.getForObject("http://contactservice/contacts", Contact[].class);
 	System.out.println(contacts.length);
 	return new ResponseEntity<Contact[]>(contacts, HttpStatus.OK);
-    }
+    }*/
 }
 
 class Contact {
